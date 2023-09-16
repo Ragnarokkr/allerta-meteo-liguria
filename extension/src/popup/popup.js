2524ad1a6d7ac307377ebffb8d667a9406a5a404
ragnarokkr <marco.trulla@gmail.com>
2023-09-16 14:45:50 +0200

    (feat): refactor popup UI
    Added missing links to Civil Protection and ARPAL websites to the popup,
    and refactored the popup UI to improve usability and accessibility.

--- extension/src/popup/popup.js
+++ extension/src/popup/popup.js
@@ -4,8 +4,10 @@
 // https://opensource.org/licenses/MIT
 
 const map = document.getElementById('map');
-const actions = document.getElementById('actions');
+const actionsPrimary = document.getElementById('actions-primary');
+const actionsSecondary = document.getElementById('actions-secondary');
 
+const { settings } = await chrome.storage.local.get(['settings']);
 const { lastUpdate } = await chrome.storage.local.get(['lastUpdate']);
 
 // loads the alert map
@@ -13,13 +15,25 @@ const image = new Image();
 image.src = lastUpdate.links.map;
 map.appendChild(image);
 
-// creates the action links
+// creates action links
+const civProtLink = document.createElement('a');
+civProtLink.href = settings.scrapeWebPageURL;
+civProtLink.textContent = chrome.i18n.getMessage('notification_button1');
+civProtLink.classList.add('action');
+actionsPrimary.appendChild(civProtLink);
+
+const arpalLink = document.createElement('a');
+arpalLink.href = settings.arpalWebsite;
+arpalLink.textContent = chrome.i18n.getMessage('notification_button2');
+arpalLink.classList.add('action');
+actionsPrimary.appendChild(arpalLink);
+
 if (lastUpdate.links.alert) {
   const alertLink = document.createElement('a');
   alertLink.href = lastUpdate.links.alert;
   alertLink.textContent = chrome.i18n.getMessage('action_button1');
   alertLink.classList.add('action');
-  actions.appendChild(alertLink);
+  actionsSecondary.appendChild(alertLink);
 }
 
 if (lastUpdate.links.forecast) {
@@ -27,13 +41,14 @@ if (lastUpdate.links.forecast) {
   forecastLink.href = lastUpdate.links.forecast;
   forecastLink.textContent = chrome.i18n.getMessage('action_button2');
   forecastLink.classList.add('action');
-  actions.appendChild(forecastLink);
+  actionsSecondary.appendChild(forecastLink);
 }
 
 // listens for clicks on the action links    
-actions.addEventListener('click', (event) => {
-  if (event.target.tagName === 'A') {
-    event.preventDefault();
-    chrome.tabs.create({ url: event.target.href });
-  }
-})
+[actionsPrimary, actionsSecondary]
+  .forEach(elm => elm.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      event.preventDefault();
+      chrome.tabs.create({ url: event.target.href });
+    }
+  }));
